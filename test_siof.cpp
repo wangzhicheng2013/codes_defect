2cddce488994a67a67f1fb8002d4d623164a14fd
wb7898 <wb7898@arcsoft.com.cn>
2024-04-24 09:56:45 +0800

    11

--- test_siof.cpp
+++ test_siof.cpp
@@ -3,9 +3,9 @@ handler global_handler; // will call load_data()
 int main() {
     // todo
     if (global_handler.data != nullptr) {
-        delete global_handler.data;
+        delete []global_handler.data;
         global_handler.data = nullptr;
     }
-    
+
     return 0;
 }
